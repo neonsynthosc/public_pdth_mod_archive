fa9305de06bbb4c1608cafe8dfc32e5349fdd447
neonsynth <87627570+neonsynthosc@users.noreply.github.com>
2025-03-15 00:02:15 +0100

    rmv

--- sleeve_selector/lua/playermanager.lua
+++
@@ -1,8 +0,0 @@
-local module = ... or D:module("sleeve_selector")
-
-local PlayerManager = module:hook_class("PlayerManager")
-module:post_hook(50, PlayerManager, "init", function(self)
-	local level = Global.level_data and Global.level_data.level_id or "bank"
-
-	self.selected_suit = D:conf("sleeve_selector_" .. level) or "default"
-end)
